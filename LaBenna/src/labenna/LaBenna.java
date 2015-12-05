d128756220dc61bf0585c0ba212436052eadcc47
Morgan Landolina <Morgan Landolina>
2015-12-05 16:27:24 +0100

    init repo

--- LaBenna/src/labenna/LaBenna.java
+++ LaBenna/src/labenna/LaBenna.java
@@ -2,34 +2,70 @@ package labenna;
 
 import java.util.ArrayList;
 import java.io.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class LaBenna {
-    ArrayList<String> cazzate = new ArrayList<>();
+    static String file = "src/cazzate.txt";
+    static Scanner scanner = new Scanner(System.in);        
     
-    public static void main(String[] args) {
-        System.out.println(leggiCazzata());
-       
+    public static void main(String[] args){
+        
+    
+        
+        System.out.println("Cosa vuoi fare?\n1) leggi una cazzata random\n2) SIMONE NE HA SPARATA UN'ALTRA, FAMMELA INSERIRE!!");
+        String scelta = scanner.next();
+        
+        switch(scelta){
+            case "1":
+                System.out.println("La massima di oggi di Simone è:");
+                System.out.println(leggiCazzata());
+                break;
+            case "2":
+                scriviCazzata();
+                break;
+            default: break;
+        }
        
     }
     
-    static public String leggiCazzata(){
+    public static String leggiCazzata(){
         String cazzata = "";
-        String file = "src/cazzate.txt";
+        
         String riga;
+        ArrayList<String> cazzate = new ArrayList<>();
         
         try{
             FileReader fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             
-            if((riga = bufferedReader.readLine()) != null) {
-                System.out.println(riga);
+            while((riga = bufferedReader.readLine()) != null) {
+                cazzate.add(riga);
             }   
+            
+            cazzata =(cazzate.get((int) (Math.random() * cazzate.size())));
+            fileReader.close();
         }catch(Exception e){
             System.out.println("Errore!");
         }
           
         
         return cazzata;
+    }
+    
+    public static void scriviCazzata(){
+        try {
+            Writer output = new BufferedWriter(new FileWriter(file, true));
+            System.out.println("Che cosa ha detto?");
+            scanner = new Scanner(System.in);
+            String cazzata = scanner.nextLine();
+            output.append(System.getProperty("line.separator"));
+            output.append("\n" + cazzata);
+            output.close();
+        } catch (IOException ex) {
+            Logger.getLogger(LaBenna.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
